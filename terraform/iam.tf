17baba0703a337c8a1ca59db38659ebd8f16b452
Lucas Madureira <jlmadureira90@gmail.com>
2026-02-01 17:18:32 -0300

    build(iam): configure least-privilege roles for eks cluster and nodes

---
+++ terraform/iam.tf
@@ -0,0 +1,70 @@
+# --- EKS Cluster Role ---
+# Allows EKS Control Plane to manage AWS resources on your behalf
+resource "aws_iam_role" "eks_cluster" {
+  name = "${var.environment}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.environment}-eks-cluster-role"
+  }
+}
+
+# Attach necessary policies to Cluster Role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+# --- EKS Node Group Role ---
+# Allows EC2 Worker Nodes to function within the cluster
+resource "aws_iam_role" "eks_nodes" {
+  name = "${var.environment}-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.environment}-eks-node-role"
+  }
+}
+
+# Attach necessary policies to Node Role
+# 1. Worker Node Policy: Allow connection to EKS
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+# 2. CNI Policy: Allow Pod networking modifications
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+# 3. Registry Read Only: Allow pulling Docker images from ECR
+resource "aws_iam_role_policy_attachment" "eks_ecr_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes.name
+}
